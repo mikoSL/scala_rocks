0b0ddb4dca40889d3e42e2096ecc76d7e8922e45
mikoSL <lina.bamboo@gmail.com>
2018-07-21 21:16:51 +0300

    Reduce a list of value into a single value.

---
+++ 2_ScalaExercises/Reducer.scala
@@ -0,0 +1,26 @@
+/**
+* Reduce a list of value into a single value by starting with an initial value
+* and combining it with each element of the list.
+*/
+
+abstract class Reducer(init: Int) {
+ def combine(x: Int, y: Int) : Int
+ def reduce(xs: List[Int]): Int =
+  xs match {
+   case Nil => init
+   case y :: ys => combine(y, reduce(ys))
+  }
+}
+
+object Product extends Reducer(1) {
+ def combine(x: Int, y: Int): Int = x * y
+}
+
+object Sum extends Reducer(0) {
+ def combine(x: Int, y: Int): Int = x + y
+}
+
+val nums = List(1,2,3,4)
+
+Product.reduce(nums) // returns 24
+Sum.reduce(nums) // returns 10
